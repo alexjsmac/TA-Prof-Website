fb4492053c779382d574f086cd04ff41141fb570
wcallag3 <wcallag3@uwo.ca>
2014-11-26 03:43:42 +0000

    nav on login

--- php/login.php
+++ php/login.php
@@ -62,6 +62,28 @@ else if (isset($_POST['username']))
             <h1 align="center">CS3319 Assignment 3</h1>
             <p align="center">By: Alex MacLean & Will Callaghan</p>
         </div>
+
+<nav class="navbar navbar-default" role="navigation">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li><a href="prof.php">Home</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
         <h2> Login to access secretary privileges:</h2>
         <div class=>
         <div class="span12">
