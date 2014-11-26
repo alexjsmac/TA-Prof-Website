e34d66a219dd99ecc7ecaf0c430fd74561a33e2d
wcallag3 <wcallag3@uwo.ca>
2014-11-26 20:32:49 +0000

    footer

--- php/prof.php
+++ php/prof.php
@@ -81,7 +81,12 @@
         </div>
       </div>
     </div>
+    <div id="fix-for-navbar-spacing" style="height: 42px;">&nbsp;</div>
+    <div class = "navbar navbar-default navbar-fixed-bottom">
+      <div class = "container">
+        <p class = "navbar-text">CS3319A Assignment 3 - Created By Alex MacLean and William Callaghan</p>
+      </div>
+    </div>
   </div>
-</div>
 </body>
 </html>                                     
